329ae0ec1ffe67696644a1c5ce9068cfaf83decd
Arjun <s10196189@connect.np.edu.sg>
2021-02-01 19:51:39 +0800

    Edited flight class

--- Booking System/Booking System/Flight.h
+++ Booking System/Booking System/Flight.h
@@ -8,13 +8,25 @@ using namespace std;
 class Flight
 {
 private:
-	string number; // Flight Number
-	string status; // Flight Status (Available, Cancelled)
-	int seats; // Number of seats left on the flight
+	string Departure;
+	string Destination;
+	int DepartureDate; //Epochs
+	int DepartureTime; //Epochs
+	string Number; // Flight Number
+	string Status; // Flight Status (Available, Cancelled)
+	int Seats; // Number of seats left on the flight
 
 public:
 	Flight();
-	Flight(string n, string st, int s);
+	Flight(string departure, string destination, int departuredate, int departuretime, string number, string status, int seats) {
+		Departure = departure;
+		Destination = destination;
+		DepartureDate = departuredate;
+		DepartureTime = departuretime;
+		Number = number;
+		Status = status;
+		Seats = seats;
+	}
 
 	void setNumber(string n);
 	string getNumber();
